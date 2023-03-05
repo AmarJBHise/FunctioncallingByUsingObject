ed5e09019bad7ca040989647fdae8bb5c98df222
bhiseamar811@gmail.com <bhiseamar811@gmail.com>
2023-03-05 08:20:01 +0530

    Add project files.

---
+++ CallFunction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functioncalling
+{
+    internal class CallFunction
+    {
+         public static void Staticfunction()
+        {
+
+        }
+        
+
+        
+        public static void Staticfunction1()
+        {
+            Staticfunction();
+
+            CallFunction ABC = new CallFunction();
+            ABC.nonstaticfuction1();               // calling Non Static Function From Static
+                                                   // Function
+            
+        }
+
+        public void nonstaticfuncation()
+        {
+            nonstaticfuction1();
+            Staticfunction1();                   // Calling Static Fuction From
+                                                // Non Static Function 
+
+        }
+
+        public void nonstaticfuction1()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Functioncalling
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+           CallFunction. Staticfunction1();
+
+
+            // Calling NonStaticFunction from Main Static Function
+           CallFunction ABC = new CallFunction();
+               ABC.nonstaticfuction1();
+
+
+        }
+        
+        
+    }
+}
